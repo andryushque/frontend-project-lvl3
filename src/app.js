dbe6a1ef25ab8da20ea4491f5eac28f35d489c3b
andryushque <andrew.ustinov23@gmail.com>
2020-02-08 20:32:34 +0300

    update: app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { watch } from 'melanke-watchjs';
 import * as yup from 'yup';
 import axios from 'axios';
-import createFeed from './renders';
+import render from './renders';
 import parse from './parser';
+
+const state = {
+  form: {
+    inputProcessState: 'inProcess',
+    validationState: true,
+    inputedUrl: '',
+  },
+  feed: {
+    currentPosts: [],
+  },
+  urlList: [],
+};
 
 export default () => {
   const inputForm = document.getElementById('url');
   const button = document.querySelector('.btn');
 
-  const state = {
-    form: {
-      inputProcessState: 'inProcess',
-      validationState: true,
-      inputedUrl: '',
-    },
-
-    urlList: [],
-  };
-
-  // validation functions
   const checkoutFeedUrlSchema = yup
     .string()
     .url('Please enter a valid URL address.')
@@ -29,7 +29,6 @@ export default () => {
   const isUrlValid = (url) => checkoutFeedUrlSchema.isValid(url).then((valid) => valid);
   const isUrlDuplicated = (url) => state.urlList.includes(url);
 
-  // events
   inputForm.addEventListener('input', (e) => {
     state.form.inputedUrl = e.target.value;
     isUrlValid(state.form.inputedUrl).then((valid) => {
@@ -50,32 +49,13 @@ export default () => {
 
     axios.get(link)
       .then((response) => {
-        const feed = parse(response.data);
-        createFeed(feed.title, feed.description, feed.feedPosts);
+        const feedData = parse(response.data);
+        state.feed.currentPosts = feedData;
       })
       .catch(console.log);
 
     state.form.inputProcessState = 'done';
   });
-
-  // watchers
-  watch(state.form, 'validationState', () => {
-    if (state.form.validationState) {
-      button.disabled = !state.form.validationState; // => false
-      inputForm.classList.remove('is-invalid');
-      inputForm.classList.add('is-valid');
-    } else {
-      button.disabled = !state.form.validationState; // => true
-      inputForm.classList.remove('is-valid');
-      inputForm.classList.add('is-invalid');
-    }
-  });
-
-  watch(state.form, 'inputProcessState', () => {
-    if (state.form.inputProcessState === 'done') {
-      inputForm.classList.remove('is-valid');
-      inputForm.value = '';
-      button.disabled = true;
-    }
-  });
 };
+
+render(state);
