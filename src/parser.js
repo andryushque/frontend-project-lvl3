de00b19efd3322930305c4931082394f2e628a45
andryushque <andrew.ustinov23@gmail.com>
2020-02-08 17:30:26 +0300

    update: parser.js

--- src/parser.js
+++ src/parser.js
@@ -4,12 +4,20 @@ const parse = (data) => {
   const parsedData = domParser.parseFromString(data, 'text/xml');
   const title = parsedData.querySelector('title').textContent;
   const description = parsedData.querySelector('description').textContent;
+
+  const feedPosts = [];
   const items = parsedData.querySelectorAll('item');
+  items.forEach((item) => {
+    const postTitle = item.querySelector('title').textContent;
+    const postLink = item.querySelector('link').textContent;
+    const feedPost = { postTitle, postLink };
+    feedPosts.push(feedPost);
+  });
 
   return {
     title,
     description,
-    items,
+    feedPosts,
   };
 };
 
