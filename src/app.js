bd0d77a7f8865d4af061e571702b988e3c4b5510
andryushque <andrew.ustinov23@gmail.com>
2020-02-08 15:14:27 +0300

    update: app.js - remove renders

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { watch } from 'melanke-watchjs';
 import * as yup from 'yup';
 import axios from 'axios';
+import { createUrlFeedList, createPostsList } from './renders';
 
 export default () => {
   const domParser = new DOMParser();
@@ -30,37 +31,6 @@ export default () => {
 
   const isUrlValid = (url) => checkoutFeedUrlSchema.isValid(url).then((valid) => valid);
   const isUrlDuplicated = (url) => state.urlList.includes(url);
-
-  const createUrlFeedList = (title, description = 'no description') => {
-    const li = document.createElement('li');
-    li.classList.add('feedListItem');
-
-    const urlTitle = document.createElement('h4');
-    urlTitle.innerText = title;
-
-    const urlDescription = document.createElement('p');
-    urlDescription.innerText = description;
-
-    li.append(urlTitle, urlDescription);
-    document.querySelector('.feedList').append(li);
-  };
-
-  const createPostsList = (itemsColl) => {
-    const items = Object.values(itemsColl);
-    console.log(items);
-
-    items.forEach((item) => {
-      const li = document.createElement('li');
-      li.classList.add('postItem');
-
-      const urlLink = document.createElement('a');
-      urlLink.href = item.querySelector('link').textContent;
-      urlLink.innerText = item.querySelector('title').textContent;
-
-      li.append(urlLink);
-      document.querySelector('.postsList').append(li);
-    });
-  };
 
   // DOMParser => https://developer.mozilla.org/ru/docs/Web/API/DOMParser
   // proxy => https://github.com/Rob--W/cors-anywhere/#documentation
