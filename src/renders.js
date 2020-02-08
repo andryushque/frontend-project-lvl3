161b986c6c29bb6dfe52d3b21140b37f16cfc44d
andryushque <andrew.ustinov23@gmail.com>
2020-02-08 20:32:49 +0300

    update: renders.js

--- src/renders.js
+++ src/renders.js
@@ -1,4 +1,8 @@
+import { watch } from 'melanke-watchjs';
+
 const body = document.querySelector('body');
+const inputForm = document.getElementById('url');
+const button = document.querySelector('.btn');
 
 const createFeed = (title, description, itemsColl) => {
   const divFeed = document.createElement('div');
@@ -19,6 +23,7 @@ const createFeed = (title, description, itemsColl) => {
   divFeedInfo.append(feedTitle, feedDescription);
 
   const items = Object.values(itemsColl);
+  // items.length = 5; // => limit?
   items.forEach((item) => {
     const { postTitle, postLink } = item;
     const li = document.createElement('li');
@@ -34,4 +39,33 @@ const createFeed = (title, description, itemsColl) => {
   body.append(divFeed);
 };
 
-export default createFeed;
+
+const render = (state) => {
+  const { form, feed } = state;
+
+  watch(form, 'validationState', () => {
+    if (form.validationState) {
+      button.disabled = !form.validationState; // => false
+      inputForm.classList.remove('is-invalid');
+      inputForm.classList.add('is-valid');
+    } else {
+      button.disabled = !form.validationState; // => true
+      inputForm.classList.remove('is-valid');
+      inputForm.classList.add('is-invalid');
+    }
+  });
+
+  watch(form, 'inputProcessState', () => {
+    if (form.inputProcessState === 'done') {
+      inputForm.classList.remove('is-valid');
+      inputForm.value = '';
+      button.disabled = true;
+    }
+  });
+
+  watch(feed, 'currentPosts', () => {
+    createFeed(feed.currentPosts.title, feed.currentPosts.description, feed.currentPosts.feedPosts);
+  }); // => fix later
+};
+
+export default render;
