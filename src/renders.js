55170227fd6c3d21bf2f865876da365c78ce6f9e
andryushque <andrew.ustinov23@gmail.com>
2020-02-09 22:59:28 +0300

    update: renders.js

--- src/renders.js
+++ src/renders.js
@@ -3,6 +3,7 @@ import { watch } from 'melanke-watchjs';
 const body = document.querySelector('body');
 const inputForm = document.getElementById('url');
 const button = document.querySelector('.btn');
+const message = document.getElementById('message');
 
 const createFeed = (title, description, itemsColl) => {
   const divFeed = document.createElement('div');
@@ -40,32 +41,53 @@ const createFeed = (title, description, itemsColl) => {
 };
 
 
+const createMessage = (resultMessage) => {
+    if (document.querySelector('.message')) {
+        document.querySelector('.message').remove();
+    };
+    const divMessage = document.createElement('div');
+    divMessage.classList.add('message', 'container', 'invalid-feedback', 'd-block');
+    divMessage.innerText = resultMessage;
+    message.append(divMessage);
+};
+
+
 const render = (state) => {
-  const { form, feed } = state;
+  const { form, feed, validateResultMessage } = state;
 
   watch(form, 'validationState', () => {
     if (form.validationState) {
       button.disabled = !form.validationState; // => false
       inputForm.classList.remove('is-invalid');
       inputForm.classList.add('is-valid');
+      createMessage(state.validateResultMessage);
     } else {
       button.disabled = !form.validationState; // => true
       inputForm.classList.remove('is-valid');
       inputForm.classList.add('is-invalid');
+      createMessage(state.validateResultMessage);
     }
   });
 
   watch(form, 'inputProcessState', () => {
     if (form.inputProcessState === 'done') {
       inputForm.classList.remove('is-valid');
+      inputForm.classList.remove('is-invalid');
       inputForm.value = '';
+      if (document.querySelector('.message')) {
+        document.querySelector('.message').remove();
+      };
+      state.validateResultMessage = '';
       button.disabled = true;
-    }
+    };
   });
 
   watch(feed, 'currentPosts', () => {
-    createFeed(feed.currentPosts.title, feed.currentPosts.description, feed.currentPosts.feedPosts);
-  }); // => fix later
+    const title = feed.currentPosts.title;
+    const description = feed.currentPosts.description;
+    const feedPosts = feed.currentPosts.feedPosts;
+    createFeed(title, description, feedPosts);
+  });
 };
 
 export default render;
