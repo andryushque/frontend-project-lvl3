772b49a57aac51c7f972fb60ace691d5f587f14b
andryushque <andrew.ustinov23@gmail.com>
2020-02-18 11:58:24 +0300

    fix: webpack.config.js - remove default entry point

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
