c2bd8607b12a5179793761962cfb6de2b9c7a4e4
andryushque <andrew.ustinov23@gmail.com>
2020-02-18 21:06:44 +0300

    update: fix namings

--- src/parser.js
+++ src/parser.js
@@ -2,22 +2,22 @@ const domParser = new DOMParser();
 
 const parse = (data) => {
   const parsedData = domParser.parseFromString(data, 'text/xml');
-  const feedTitle = parsedData.querySelector('title').textContent;
-  const feedDescription = parsedData.querySelector('description').textContent;
+  const title = parsedData.querySelector('title').textContent;
+  const description = parsedData.querySelector('description').textContent;
 
-  const feedPosts = [];
+  const posts = [];
   const postItems = parsedData.querySelectorAll('item');
   postItems.forEach((postItem) => {
     const postTitle = postItem.querySelector('title').textContent;
     const postLink = postItem.querySelector('link').textContent;
-    const feedPost = { postTitle, postLink };
-    feedPosts.push(feedPost);
+    const post = { postTitle, postLink };
+    posts.push(post);
   });
 
   return {
-    feedTitle,
-    feedDescription,
-    feedPosts,
+    title,
+    description,
+    posts,
   };
 };
 
