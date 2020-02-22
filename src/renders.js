c10752132679f4de7c87ad4f176bbf70b09594df
andryushque <andrew.ustinov23@gmail.com>
2020-02-22 12:01:00 +0300

    update: fix rendering

--- src/renders.js
+++ src/renders.js
@@ -1,63 +1,62 @@
 import { watch } from 'melanke-watchjs';
 import _ from 'lodash';
 
-const inputField = document.getElementById('url');
-const button = document.querySelector('.btn');
-const message = document.getElementById('message');
-const channelsList = document.querySelector('.channelsList');
-const postsList = document.querySelector('.postsList');
-const postsCount = document.getElementById('postsCount');
-
-const renderChannelsList = (channels) => {
-  channels.forEach((channel) => {
-    const { title, description } = channel;
-    const channelTitle = document.createElement('h5');
-    channelTitle.innerText = title;
-    const channelDescription = document.createElement('div');
-    channelDescription.classList.add('text-secondary');
-    channelDescription.innerText = description;
-
-    const channelItem = document.createElement('div');
-    const underline = document.createElement('hr');
-    channelItem.classList.add('mb-4');
-    channelItem.id = _.uniqueId('channel_');
-    channelItem.append(channelTitle, channelDescription);
-    channelsList.prepend(channelItem, underline);
-  });
-};
-
-const renderPostsList = (posts) => {
-  posts.forEach((post) => {
-    const { postTitle, postLink } = post;
-    const postItemLink = document.createElement('a');
-    const postItem = document.createElement('li');
-    postItemLink.innerText = postTitle;
-    postItemLink.href = postLink;
-    postItem.classList.add('feedPost');
-    postItem.id = _.uniqueId('post_');
-    postItem.append(postItemLink);
-    postsList.prepend(postItem);
-  });
-};
-
-const renderPostsCount = (count) => {
-  const current = Number(postsCount.innerText);
-  postsCount.innerText = current + count;
-  postsCount.classList.add('text-dark');
-};
-
-const renderErrorMessage = (errorMessage, feedbackTypeClass) => {
-  if (document.querySelector('.errorMessage')) {
-    document.querySelector('.errorMessage').remove();
-  }
-  const errorMessageContainer = document.createElement('div');
-  errorMessageContainer.classList.add('errorMessage', 'container', 'd-block', 'mt-2', feedbackTypeClass);
-  errorMessageContainer.innerText = errorMessage;
-  message.append(errorMessageContainer);
-};
-
-const render = (state) => {
+export default (state) => {
   const { form } = state;
+  const inputField = document.getElementById('url');
+  const button = document.querySelector('.btn');
+  const message = document.getElementById('message');
+  const channelsList = document.querySelector('.channelsList');
+  const postsList = document.querySelector('.postsList');
+  const postsCount = document.getElementById('postsCount');
+
+  const renderChannelsList = (channels) => {
+    channels.forEach((channel) => {
+      const { title, description } = channel;
+      const channelTitle = document.createElement('h5');
+      channelTitle.innerText = title;
+      const channelDescription = document.createElement('div');
+      channelDescription.classList.add('text-secondary');
+      channelDescription.innerText = description;
+
+      const channelItem = document.createElement('div');
+      const underline = document.createElement('hr');
+      channelItem.classList.add('mb-4');
+      channelItem.id = _.uniqueId('channel_');
+      channelItem.append(channelTitle, channelDescription);
+      channelsList.prepend(channelItem, underline);
+    });
+  };
+
+  const renderPostsList = (posts) => {
+    posts.forEach((post) => {
+      const { postTitle, postLink } = post;
+      const postItemLink = document.createElement('a');
+      const postItem = document.createElement('li');
+      postItemLink.innerText = postTitle;
+      postItemLink.href = postLink;
+      postItem.classList.add('feedPost');
+      postItem.id = _.uniqueId('post_');
+      postItem.append(postItemLink);
+      postsList.prepend(postItem);
+    });
+  };
+
+  const renderPostsCount = (count) => {
+    const current = Number(postsCount.innerText);
+    postsCount.innerText = current + count;
+    postsCount.classList.add('text-dark');
+  };
+
+  const renderErrorMessage = (errorMessage, feedbackTypeClass) => {
+    if (document.querySelector('.errorMessage')) {
+      document.querySelector('.errorMessage').remove();
+    }
+    const errorMessageContainer = document.createElement('div');
+    errorMessageContainer.classList.add('errorMessage', 'container', 'd-block', 'mt-2', feedbackTypeClass);
+    errorMessageContainer.innerText = errorMessage;
+    message.append(errorMessageContainer);
+  };
 
   watch(state, 'form', () => {
     if (form.inputProcessState === 'done') {
@@ -102,5 +101,3 @@ const render = (state) => {
     renderPostsCount(state.posts.length);
   });
 };
-
-export default render;
